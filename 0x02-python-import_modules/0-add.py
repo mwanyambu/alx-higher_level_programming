373d83cd0dcda6b3d9a6d3d4350db2c82dca39ff
mwanyambu <jmwanyambu@gmail.com>
2023-08-10 14:01:22 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 from add_0 import add
+"""import the add function from the add_0 module to perform addition"""
+
+
 def main():
     a = 1
     b = 2
     print("{0} + {1} = {2}".format(a, b, add(a, b)))
+
+
 if __name__ == "__main__":
     main()
