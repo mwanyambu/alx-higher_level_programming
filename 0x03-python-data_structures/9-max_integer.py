fb56300263ee137c6f27d0a5634d12a681fc62a1
mwanyambu <jmwanyambu@gmail.com>
2023-08-14 16:42:46 -0700

    data structures

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,6 +5,6 @@ def max_integer(my_list=[]):
     else:
         max_int = my_list[0]
         for i in my_list:
-            if i > max_val:
+            if i > max_int:
                 max_int = i
         return max_int
