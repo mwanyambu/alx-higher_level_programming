33646dd622427c20d2dfe934ccdd095d88115417
mwanyambu <jmwanyambu@gmail.com>
2023-08-14 16:33:20 -0700

    data structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
+
 def multiple_returns(sentence):
     if sentence == "":
-        return None, 0
+        return (None, 0)
     else:
-        return len(sentence), sentence[0]
+        return (len(sentence), sentence[0])
