989ab7c832917bb65202f184243abcbbea7cfaf7
mwanyambu <jmwanyambu@gmail.com>
2023-08-14 16:11:53 -0700

    data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = my_string.replace('c', '').replace('C', '')
+    new = ""
+    for ch in my_string:
+        if ch != 'c' and ch != 'C':
+            new += ch
     return new
