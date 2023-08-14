65f033684a70692d97529153aa1aff748c9d978b
mwanyambu <jmwanyambu@gmail.com>
2023-08-14 15:55:19 -0700

    data structures

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,9 +2,10 @@
 
 
 def print_reversed_list_integer(my_list=[]):
+    if my_list == "":
+        return None
+
     my_list.reverse()
     for i in my_list:
         print("{:d}".format(i))
 
-    if my_list is None:
-        return 
