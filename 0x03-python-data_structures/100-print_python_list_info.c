313ed2163b5748f76263e9ae172a50caa0bd28fe
mwanyambu <jmwanyambu@gmail.com>
2023-08-14 16:51:50 -0700

    data structures

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,21 +1,22 @@
-#include <python.h>
+#include <Python.h>
 /**
  * print_python_list_info - info of a python list
  * @p: python list
+ * Return: bit index else -1
  */
-void print_python_list_info(Py0bject *p)
+void print_python_list_info(PyObject *p)
 {
-	Py0bject *lst_object;
+	PyObject *lst_object;
 	Py_ssize_t lst_size, x;
 
-	lst_size= Pylist_size(p);
+	lst_size= Pylist_Size(p);
 	if (lst_size < 0)
 	{
 		printf("Failed to get the size of the list\n");
 		return;
 	}
 	printf("[*] Size of python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", ((PyList0bject *)p)->allocated);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 	for (x = 0; x < lst_size; x++)
 	{
 		printf("Element %ld", x);
@@ -26,6 +27,6 @@ void print_python_list_info(Py0bject *p)
 			printf("Failed to get an item at index %ld\n", x);
 			continue;
 		}
-		printf("Type: %s\n", lst_object->obj_type->tp_name);
+		printf("%s\n", lst_object->ob_type->tp_name);
 	}
 }
