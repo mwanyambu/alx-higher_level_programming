0931f7039932d05d67f58ccb145f869f42a4bfc4
mwanyambu <jmwanyambu@gmail.com>
2023-08-29 15:36:48 -0700

    classes

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+class Square:
+
+    def area(self):
+        return self.__size * self.__size
+    def size(self):
+        return self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+                print("#" * self.__size)
