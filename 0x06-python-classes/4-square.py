ad609e8caf9e3e23e7d2506271b5e042e5d84a35
mwanyambu <jmwanyambu@gmail.com>
2023-08-30 01:47:55 -0700

    correction of 4-square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
+
+"""a class square that defines a square"""
+
+
 class Square:
 
     def area(self):
+        """calculates the area of a square"""
         return self.__size * self.__size
+
     def size(self):
+        """size of the square"""
         return self.__size
 
     @property
     def size(self):
+        """property to retrieve size"""
         return self.__size
 
     def __init__(self, size=0):
+        """initialization of size"""
         self.__size = size
 
     @size.setter
     def size(self, value):
+        """"
+        set size
+        size must be an integer and not less than zero
+        value must be an integer and not less than zero
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
