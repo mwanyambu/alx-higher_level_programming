4d691b95a9d6bfe710778775c0ca6b676aa5ebc4
mwanyambu <jmwanyambu@gmail.com>
2023-08-30 06:32:32 -0700

    classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,7 +10,7 @@ class Square:
         return self.__size * self.__size
 
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
