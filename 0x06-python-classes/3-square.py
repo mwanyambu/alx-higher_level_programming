e774a9eb7e146b9beb5107247bd901ab4ddf7f77
mwanyambu <jmwanyambu@gmail.com>
2023-08-30 01:31:36 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 
 """a class square that defines a square"""
+
+
 class Square:
-
-    """
-    area calculates the area of a square
-    size: private instance attribute
-    """
-
+    """size: private instance attribute"""
     def area(self):
+        """area is the area of the square"""
         return self.__size * self.__size
 
     def __init__(self, size=0):
-
-
+        """size must be an integer and not less than 0"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
