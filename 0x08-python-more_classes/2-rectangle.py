1f885b354dbedc460407ec70b3539cd46e211600
mwanyambu <jmwanyambu@gmail.com>
2023-09-04 14:48:57 -0700

    classes

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -36,8 +36,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """initialize width and height"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     def area(self):
         """returns the area of a rectangle"""
