ce32d17a61c761b85ecc761dc962f9866119b20d
mwanyambu <jmwanyambu@gmail.com>
2023-10-18 15:19:24 -0700

    joins

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- script joins two tables
 SELECT name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
-JOIN tv_show_genres ON id=tv_show_genres.show_id
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
-WHERE tv_show_genres.show_id IS NULL
 ORDER BY number_of_shows DESC;
