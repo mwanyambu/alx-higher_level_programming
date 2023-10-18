179b91698ceba7e623d8fac8698d5c99902df946
mwanyambu <jmwanyambu@gmail.com>
2023-10-18 14:58:34 -0700

    queries

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_shows_genres.genre_id = tv genres.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY title, tv_genres.name;
