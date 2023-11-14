c8be463aeabf066ce6e33f483006910182f6cfa1
mwanyambu <jmwanyambu@gmail.com>
2023-11-14 14:02:22 -0800

    factorial

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,12 +3,10 @@
 function add(a, b) {
   return a + b;
 }
-if (process.argv.length > 3) {
-  const num1 = parseInt(process.argv[2]);
-  const num2 = parseInt(process.argv[3]);
-  if (!isNaN(num1) && !isNaN(num2)) {
-    console.log(add(num1, num2));
-  } else {
-    console.log('NaN');
-  }
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+if (!isNaN(num1) && !isNaN(num2)) {
+  console.log(add(num1, num2));
+} else {
+  console.log('NaN');
 }
