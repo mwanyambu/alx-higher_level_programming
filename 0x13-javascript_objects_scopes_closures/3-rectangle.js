8a53879105d4884447f07139864ce0de4653b406
mwanyambu <jmwanyambu@gmail.com>
2023-11-16 13:32:43 -0800

    classes

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,7 +7,8 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
-  print() {
+ 
+  print () {
     for (let y = 0; y < this.height; y++) {
       console.log('X'.repeat(this.width));
     }
