7e7bc4190cb2e1093fc0ccff220f40bf03d85f56
mwanyambu <jmwanyambu@gmail.com>
2023-11-16 13:25:55 -0800

    classes

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,7 +4,7 @@ module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
-      this.height  = h;
+      this.height = h;
     }
-  }
-}
+  };
+};
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,9 +4,9 @@ module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
-      this.height  = h;
+      this.height = h;
     }
-  }
+  };
   print() {
     for (let y = 0; y < this.height; y++) {
       console.log('X'.repeat(this.width));
